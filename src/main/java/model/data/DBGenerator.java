ac06bddd4bd8d8263ad4b73bf5771395e3df49a3
Brokolito <m.castro14@ufromail.cl>
2023-05-18 17:58:36 -0400

    Se agregan tablas al generador de la BDD

--- src/main/java/model/data/DBGenerator.java
+++ src/main/java/model/data/DBGenerator.java
@@ -15,7 +15,9 @@ public class DBGenerator {
         create = actualizarConexion(connection,nombreBD);
         crearTablaProductos(create);
         crearTablaTrabajadores(create);
-        //relacionarTabla(create,"Estudiante","codigo_carrera","Carrera");
+        crearTablaFechaDeVencimiento(create);
+        crearTablaCompras(create);
+        crearTablaProductosComprados(create);
         DBConnector.closeConnection();
     }
     //Metodo para conectarse a una base de datos ya creada
@@ -24,18 +26,15 @@ public class DBGenerator {
         DSLContext create = DSL.using(connection);
         return create;
     }
-    //Crea una base de datos en caso de que no exista
     private static void crearBaseDato(DSLContext create, String nombreBD){
         create.createDatabaseIfNotExists(nombreBD).execute();
     }
-    //Actualiza la conexion inicial para conectar a la base de datos
     private static DSLContext actualizarConexion(Connection connection,String nombreBD){
         DBConnector.closeConnection();
         connection= DBConnector.connection(nombreBD,"root","");
         DSLContext create=DSL.using(connection);
         return create;
     }
-    //Método estandar para crear una tabla
     private static void crearTablaProductos(DSLContext create){
         create.createTableIfNotExists(
                 "productos").
@@ -44,21 +43,34 @@ public class DBGenerator {
                 .column("stock",INTEGER)
                 .column("precio",INTEGER).execute();
     }
-    //Método estandar para crear una tabla
     private static void crearTablaTrabajadores(DSLContext create){
-        create.createTableIfNotExists("Estudiante").column("rut",VARCHAR(50))
+        create.createTableIfNotExists("trabajadores").column("rut",VARCHAR(50))
                 .column("nombre",VARCHAR(100))
-                .column("matricula",VARCHAR(50))
-                .column("codigo_carrera",VARCHAR(50))
+                .column("cargo",VARCHAR(100))
+                .column("registro_usuario",TIMESTAMP)
                 .constraint(primaryKey("rut")).execute();
     }
-    //Relaciona dos tablas a traves de una clave foranea
-    private static void relacionarTabla(DSLContext create, String nombreTabla, String claveForanea, String
-            nombreTablaRelacion){
-        create.alterTableIfExists(nombreTabla).alterConstraint(foreignKey(claveForanea).references(nombreTablaRelacion)).enforced();
+    public static void crearTablaFechaDeVencimiento(DSLContext create){
+        create.createTableIfNotExists("fecha_vencimiento_productos").column("id",INTEGER)
+                .constraint(foreignKey("producto_id").references("productos","id"))
+                .column("cantidad",INTEGER)
+                .column("fecha_vencimiento",DATE)
+                .execute();
     }
-    private static void agregarColumnaTabla(DSLContext create, String nombreTabla, String columna, DataType
-            tipoColumna){
-        create.alterTableIfExists(nombreTabla).addColumn(columna,tipoColumna);
+    public static void crearTablaCompras(DSLContext create){
+        create.createTableIfNotExists("compras").column("id",INTEGER.identity(true))
+                .constraint(primaryKey("id")).column("estado",VARCHAR(100))
+                .column("rut_vendedor",VARCHAR(50))
+                .column("fecha_compra",TIMESTAMP)
+                .column("total_pagado",INTEGER)
+                .constraint(foreignKey("rut_vendedor")
+                        .references("trabajadores","rut"))
+                .execute();
+    }
+    public static void crearTablaProductosComprados(DSLContext create){
+        create.createTableIfNotExists("productos_comprados").column("compra_id",INTEGER)
+                .constraint(foreignKey("compra_id").references("compras","id")).
+                column("id_producto",INTEGER).constraint(foreignKey("id_producto").references("productos","id"))
+                .execute();
     }
 }
